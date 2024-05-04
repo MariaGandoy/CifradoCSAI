8e8a70e07b86e6a6ddd20e3cc2c8b8a2b9df47b8
María Gandoy <maria.gandoy@udc.es>
2024-05-04 11:56:22 +0200

    Funciona desde linea de comandos y entorno virtual

--- vigenere.py
+++ vigenere.py
@@ -1,21 +1,47 @@
 #https://github.com/Taloohi/Vigenere-Cipher                             -- Obtención de la longitud de la llave
 
-#https://github.com/savanddarji/Cracking-a-Vigenere-Cipher/tree/master  -- Obtención de la llave
+#https://github.com/savanddarji/Cracking-a-Vigenere-Cipher/tree/master  -- Obtención de la llave y base general
 
 
 from __future__ import division
 from math import gcd
 import string
 import numpy as np
+import argparse
+
+
 def shift(l1,n1): # for left shift operation
     return l1[n1:] + l1[:n1]
 num = dict(zip(range(0,26),string.ascii_lowercase))# for reverse mapping: numbers to letter
-A = [0.08167,0.01492,0.02782,0.04253,0.12702,0.02228,0.02015,0.06094,0.06966,0.00153,0.00772,0.04025,0.02406,0.06749,     0.07507,0.01929,0.00095,0.05987,0.06327,0.09056,0.02758,0.00978,0.0236,0.0015,0.01974,0.00074]
-a9=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-a=input('Please enter your vigener cipher text:').lower()
+A = [0.08167,0.01492,0.02782,0.04253,0.12702,0.02228,0.02015,0.06094,0.06966,0.00153,0.00772,0.04025,0.02406,0.06749,
+     0.07507,0.01929,0.00095,0.05987,0.06327,0.09056,0.02758,0.00978,0.0236,0.0015,0.01974,0.00074]
+
+a9=['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+    'o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+##Pillar archivo desde línea de comandos
+
+parser = argparse.ArgumentParser(description="Lee un texto cifrado desde un archivo")
+parser.add_argument(
+    "archivo",
+    type=str,
+    help="Nombre del archivo con el texto cifrado"
+)
+
+args = parser.parse_args()
+
+
+try:
+    with open(args.archivo, 'r') as archivo:
+        a = archivo.read().lower()
+except FileNotFoundError:
+    print("Archivo no encontrado.")
+
 a=list(a)
 
 #####################    GET LENGTH KEY     ##########################
+##### Inicia código de #https://github.com/Taloohi/Vigenere-Cipher 
 text=a
 
 displacement = []
@@ -48,7 +74,7 @@ def gcd(x, y):
 
 length = gcd(N.index(first),N.index(second))
 
-print("Longitud: " + str(length))
+##### Fin código de #https://github.com/Taloohi/Vigenere-Cipher 
 
 L=length
 #####################    dividing in L(max d) parts     ##########################
@@ -96,4 +122,4 @@ while v1<L:
 
 
 
-print ('The Encryption Key:',''.join(Array))
+print ('LLAVE:',''.join(Array))
